bd4983b9967049e99085affa0ff73d22bcb283b1
Joshua Batty <joshpbatty@gmail.com>
2025-10-17 10:44:22 +1100

    Refactor ofxFileHelper to use std::filesystem, improving file and directory operations with error handling.

--- eyeWriterTracker/src/addons/ofxFileHelper/ofxFileHelper.h
+++ eyeWriterTracker/src/addons/ofxFileHelper/ofxFileHelper.h
@@ -24,184 +24,390 @@
 
 #pragma once
 
-#include "ofMain.h"
+#include <filesystem>
+#include <string>
 
-#include "Poco/Util/FilesystemConfiguration.h"
-#include "Poco/File.h"
-#include "Poco/Path.h"
-#include "Poco/DirectoryIterator.h"
-#include "Poco/StringTokenizer.h"
-#include "Poco/Exception.h"
-#include "Poco/FileStream.h"
+#include "ofFileUtils.h"
+#include "ofLog.h"
 
-
-using Poco::Path;
-using Poco::File;
-using Poco::DirectoryIterator;
-using Poco::StringTokenizer;
-using Poco::NotFoundException;
+namespace fs = std::filesystem;
+using std::string;
 
 class ofxFileHelper{
-	public:
+  public:
+  static bool copyFromTo(string pathSrc, string pathDst, bool bRelativeToData = true, bool overwrite = false){
+	  if(bRelativeToData){
+		  pathSrc = ofToDataPath(pathSrc);
+		  pathDst = ofToDataPath(pathDst);
+	  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static bool copyFromTo(string pathSrc, string pathDst, bool bRelativeToData = true,  bool overwrite = false){
-		if( bRelativeToData ) pathSrc = ofToDataPath(pathSrc);
-		if( bRelativeToData ) pathDst = ofToDataPath(pathDst);
+	  fs::path src(pathSrc);
+	  fs::path dst(pathDst);
 
-		if( !ofxFileHelper::doesFileExist(pathSrc) ){
-            return false;
-		}
+	  if(!fs::exists(src)){
+		  return false;
+	  }
 
-        if( ofxFileHelper::doesFileExist(pathDst) ){
-            if( overwrite ){
-                ofxFileHelper::deleteFolder(pathDst);
-            }else{
-                return false;
-            }
-        }
+	  if(fs::exists(dst)){
+		  if(overwrite){
+			  if(!deleteFolder(pathDst, false)){
+				  return false;
+			  }
+		  }else{
+			  return false;
+		  }
+	  }
 
-		File fileSrc(pathSrc);
-		try{
-            fileSrc.copyTo(pathDst);
-		}catch (Poco::Exception & except){
-		    ofLog(OF_LOG_ERROR, "copyFromTo - unable to copy to, file must be busy");
-		    return false;
-		}
-		return true;
-	}
+	  std::error_code ec;
 
-	//be careful with slashes here - appending a slash when moving a folder will causes mad headaches
-	//------------------------------------------------------------------------------------------------------------
-	static bool moveFromTo(string pathSrc, string pathDst, bool bRelativeToData = true, bool overwrite = false){
-		if( bRelativeToData ) pathSrc = ofToDataPath(pathSrc);
-		if( bRelativeToData ) pathDst = ofToDataPath(pathDst);
+	  if(fs::is_directory(src)){
+		  fs::create_directories(dst, ec);
+		  if(ec){
+			  ofLogError("ofxFileHelper") << "copyFromTo - unable to ensure destination directory '" << dst.string()
+										  << "': " << ec.message();
+			  return false;
+		  }
+		  ec.clear();
+		  fs::copy(src, dst, fs::copy_options::recursive | fs::copy_options::copy_symlinks, ec);
+		  if(ec){
+			  ofLogError("ofxFileHelper") << "copyFromTo - unable to copy directory '" << src.string()
+										  << "' to '" << dst.string() << "': " << ec.message();
+			  return false;
+		  }
+	  }else{
+		  auto parent = dst.parent_path();
+		  if(!parent.empty()){
+			  fs::create_directories(parent, ec);
+			  if(ec){
+				  ofLogError("ofxFileHelper") << "copyFromTo - unable to create parent directory '" << parent.string()
+											  << "': " << ec.message();
+				  return false;
+			  }
+			  ec.clear();
+		  }
+		  fs::copy_file(src, dst, fs::copy_options::none, ec);
+		  if(ec){
+			  ofLogError("ofxFileHelper") << "copyFromTo - unable to copy file '" << src.string()
+										  << "' to '" << dst.string() << "': " << ec.message();
+			  return false;
+		  }
+	  }
 
-        if( !ofxFileHelper::doesFileExist(pathSrc) ){
-            return false;
-		}
+	  return true;
+  }
 
-        if( ofxFileHelper::doesFileExist(pathDst) ){
-            if( overwrite ){
-                ofxFileHelper::deleteFolder(pathDst);
-            }else{
-                return false;
-            }
-        }
+  static bool moveFromTo(string pathSrc, string pathDst, bool bRelativeToData = true, bool overwrite = false){
+	  if(bRelativeToData){
+		  pathSrc = ofToDataPath(pathSrc);
+		  pathDst = ofToDataPath(pathDst);
+	  }
 
-		File fileSrc(pathSrc);
+	  fs::path src(pathSrc);
+	  fs::path dst(pathDst);
 
-        try{
-            fileSrc.moveTo(pathDst);
-		}catch (Poco::Exception & except){
-		    ofLog(OF_LOG_ERROR, "moveFromTo - unable to move to, file must be busy");
-		    return false;
-		}
-        return true;
-	}
+	  if(!fs::exists(src)){
+		  return false;
+	  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static string getFileExt(string filename){
-		std::string::size_type idx;
-		idx = filename.rfind('.');
+	  if(fs::exists(dst)){
+		  if(overwrite){
+			  if(!deleteFolder(pathDst, false)){
+				  return false;
+			  }
+		  }else{
+			  return false;
+		  }
+	  }
 
-		if(idx != std::string::npos){
-			return filename.substr(idx+1);
-		}
-		else{
-			return "";
-		}
-	}
+	  std::error_code ec;
+	  auto parent = dst.parent_path();
+	  if(!parent.empty()){
+		  fs::create_directories(parent, ec);
+		  if(ec){
+			  ofLogError("ofxFileHelper") << "moveFromTo - unable to create destination directory '" << parent.string()
+										  << "': " << ec.message();
+			  return false;
+		  }
+		  ec.clear();
+	  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static string removeExt(string filename){
-		std::string::size_type idx;
-		idx = filename.rfind('.');
+	  fs::rename(src, dst, ec);
+	  if(!ec){
+		  return true;
+	  }
 
-		if(idx != std::string::npos){
-			return filename.substr(0, idx);
-		}
-		else{
-			return filename;
-		}
-	}
+	  ofLogWarning("ofxFileHelper") << "moveFromTo - rename failed (" << ec.message()
+									<< "), attempting copy/remove fallback";
 
-	//------------------------------------------------------------------------------------------------------------
-	static string prependBackslash(string path){
-		if(path.length() > 0){
-			if( path[0] != '/'){
-				path = "/"+path;
-			}
-		}
-		return path;
-	}
+	  if(!copyFromTo(pathSrc, pathDst, false, overwrite)){
+		  return false;
+	  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static string appendBackslash(string path){
-		if(path.length() > 0){
-			if( path[path.length()-1] != '/'){
-				path += "/";
-			}
-		}
-		return path;
-	}
+	  if(!deleteFolder(pathSrc, false)){
+		  ofLogWarning("ofxFileHelper") << "moveFromTo - copied to destination but failed to remove original '"
+										<< src.string() << "'";
+		  return false;
+	  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static bool doesDirectoryExist(string dirPath, bool bRelativeToData = true){
-		if( bRelativeToData ) dirPath = ofToDataPath(dirPath);
-		File myFile( dirPath );
-		return myFile.exists();
-	}
+	  return true;
+  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static bool doesFileExist(string fPath,  bool bRelativeToData = true){
-		if( bRelativeToData ) fPath = ofToDataPath(fPath);
-		File myFile( fPath );
-		return myFile.exists();
-	}
+  static string getFileExt(const string& filename){
+	  std::string::size_type idx = filename.rfind('.');
+	  return (idx != std::string::npos) ? filename.substr(idx + 1) : "";
+  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static bool makeDirectory(string dirPath, bool bRelativeToData = true){
-		if( bRelativeToData ) dirPath = ofToDataPath(dirPath);
+  static string removeExt(const string& filename){
+	  std::string::size_type idx = filename.rfind('.');
+	  return (idx != std::string::npos) ? filename.substr(0, idx) : filename;
+  }
 
-		File myFile(dirPath);
-		bool success = false;
-		try{
-            success = myFile.createDirectory();
-		}catch( Poco::Exception &except ){
-            ofLog(OF_LOG_ERROR, "makeDirectory - directory could not be created");
-            return false;
-		}
+  static string prependBackslash(string path){
+	  if(!path.empty() && path.front() != '/'){
+		  path.insert(path.begin(), '/');
+	  }
+	  return path;
+  }
 
-        if(!success)ofLog(OF_LOG_WARNING, "makeDirectory - directory already exists");
+  static string appendBackslash(string path){
+	  if(!path.empty() && path.back() != '/'){
+		  path.push_back('/');
+	  }
+	  return path;
+  }
 
-		return success;
-	}
+  static bool doesDirectoryExist(string dirPath, bool bRelativeToData = true){
+	  if(bRelativeToData) dirPath = ofToDataPath(dirPath);
+	  std::error_code ec;
+	  return fs::exists(dirPath, ec) && !ec;
+  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static bool deleteFile(string path, bool bRelativeToData = true){
-	    if( bRelativeToData ) path = ofToDataPath(path);
-		File myFile(path);
-		try{
-            myFile.remove();
-		}catch( Poco::Exception &except){
-            ofLog(OF_LOG_ERROR, "deleteFile - file could not be deleted");
-            return false;
-		}
-		return true;
-	}
+  static bool doesFileExist(string fPath, bool bRelativeToData = true){
+	  if(bRelativeToData) fPath = ofToDataPath(fPath);
+	  std::error_code ec;
+	  return fs::exists(fPath, ec) && !ec;
+  }
 
-	//------------------------------------------------------------------------------------------------------------
-	static bool deleteFolder(string path, bool bRelativeToData = true){
-	    if( bRelativeToData ) path = ofToDataPath(path);
-		File myFile(path);
-		try{
-            myFile.remove(true);
-		}catch(Poco::Exception &except){
-            ofLog(OF_LOG_ERROR, "deleteFile - folder could not be deleted");
-            return false;
-		}
-		return true;
-	}
+  static bool makeDirectory(string dirPath, bool bRelativeToData = true){
+	  if(bRelativeToData) dirPath = ofToDataPath(dirPath);
+	  std::error_code ec;
+	  bool created = fs::create_directories(dirPath, ec);
+	  if(ec){
+		  ofLogError("ofxFileHelper") << "makeDirectory - directory could not be created: " << ec.message();
+		  return false;
+	  }
+	  if(!created){
+		  ofLogWarning("ofxFileHelper") << "makeDirectory - directory already exists";
+	  }
+	  return created;
+  }
 
+  static bool deleteFile(string path, bool bRelativeToData = true){
+	  if(bRelativeToData) path = ofToDataPath(path);
+	  std::error_code ec;
+	  bool removed = fs::remove(path, ec);
+	  if(ec || !removed){
+		  ofLogError("ofxFileHelper") << "deleteFile - file could not be deleted"
+									  << (ec ? (": " + ec.message()) : "");
+		  return false;
+	  }
+	  return true;
+  }
+
+  static bool deleteFolder(string path, bool bRelativeToData = true){
+	  if(bRelativeToData) path = ofToDataPath(path);
+	  std::error_code ec;
+	  auto removed = fs::remove_all(path, ec);
+	  if(ec || removed == 0){
+		  ofLogError("ofxFileHelper") << "deleteFolder - path could not be deleted"
+									  << (ec ? (": " + ec.message()) : "");
+		  return false;
+	  }
+	  return true;
+  }
 };
+
+//
+//#pragma once
+//
+//#include "ofMain.h"
+//
+//#include "Poco/Util/FilesystemConfiguration.h"
+//#include "Poco/File.h"
+//#include "Poco/Path.h"
+//#include "Poco/DirectoryIterator.h"
+//#include "Poco/StringTokenizer.h"
+//#include "Poco/Exception.h"
+//#include "Poco/FileStream.h"
+//
+//
+//using Poco::Path;
+//using Poco::File;
+//using Poco::DirectoryIterator;
+//using Poco::StringTokenizer;
+//using Poco::NotFoundException;
+//
+//class ofxFileHelper{
+//	public:
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static bool copyFromTo(string pathSrc, string pathDst, bool bRelativeToData = true,  bool overwrite = false){
+//		if( bRelativeToData ) pathSrc = ofToDataPath(pathSrc);
+//		if( bRelativeToData ) pathDst = ofToDataPath(pathDst);
+//
+//		if( !ofxFileHelper::doesFileExist(pathSrc) ){
+//            return false;
+//		}
+//
+//        if( ofxFileHelper::doesFileExist(pathDst) ){
+//            if( overwrite ){
+//                ofxFileHelper::deleteFolder(pathDst);
+//            }else{
+//                return false;
+//            }
+//        }
+//
+//		File fileSrc(pathSrc);
+//		try{
+//            fileSrc.copyTo(pathDst);
+//		}catch (Poco::Exception & except){
+//		    ofLog(OF_LOG_ERROR, "copyFromTo - unable to copy to, file must be busy");
+//		    return false;
+//		}
+//		return true;
+//	}
+//
+//	//be careful with slashes here - appending a slash when moving a folder will causes mad headaches
+//	//------------------------------------------------------------------------------------------------------------
+//	static bool moveFromTo(string pathSrc, string pathDst, bool bRelativeToData = true, bool overwrite = false){
+//		if( bRelativeToData ) pathSrc = ofToDataPath(pathSrc);
+//		if( bRelativeToData ) pathDst = ofToDataPath(pathDst);
+//
+//        if( !ofxFileHelper::doesFileExist(pathSrc) ){
+//            return false;
+//		}
+//
+//        if( ofxFileHelper::doesFileExist(pathDst) ){
+//            if( overwrite ){
+//                ofxFileHelper::deleteFolder(pathDst);
+//            }else{
+//                return false;
+//            }
+//        }
+//
+//		File fileSrc(pathSrc);
+//
+//        try{
+//            fileSrc.moveTo(pathDst);
+//		}catch (Poco::Exception & except){
+//		    ofLog(OF_LOG_ERROR, "moveFromTo - unable to move to, file must be busy");
+//		    return false;
+//		}
+//        return true;
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static string getFileExt(string filename){
+//		std::string::size_type idx;
+//		idx = filename.rfind('.');
+//
+//		if(idx != std::string::npos){
+//			return filename.substr(idx+1);
+//		}
+//		else{
+//			return "";
+//		}
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static string removeExt(string filename){
+//		std::string::size_type idx;
+//		idx = filename.rfind('.');
+//
+//		if(idx != std::string::npos){
+//			return filename.substr(0, idx);
+//		}
+//		else{
+//			return filename;
+//		}
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static string prependBackslash(string path){
+//		if(path.length() > 0){
+//			if( path[0] != '/'){
+//				path = "/"+path;
+//			}
+//		}
+//		return path;
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static string appendBackslash(string path){
+//		if(path.length() > 0){
+//			if( path[path.length()-1] != '/'){
+//				path += "/";
+//			}
+//		}
+//		return path;
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static bool doesDirectoryExist(string dirPath, bool bRelativeToData = true){
+//		if( bRelativeToData ) dirPath = ofToDataPath(dirPath);
+//		File myFile( dirPath );
+//		return myFile.exists();
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static bool doesFileExist(string fPath,  bool bRelativeToData = true){
+//		if( bRelativeToData ) fPath = ofToDataPath(fPath);
+//		File myFile( fPath );
+//		return myFile.exists();
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static bool makeDirectory(string dirPath, bool bRelativeToData = true){
+//		if( bRelativeToData ) dirPath = ofToDataPath(dirPath);
+//
+//		File myFile(dirPath);
+//		bool success = false;
+//		try{
+//            success = myFile.createDirectory();
+//		}catch( Poco::Exception &except ){
+//            ofLog(OF_LOG_ERROR, "makeDirectory - directory could not be created");
+//            return false;
+//		}
+//
+//        if(!success)ofLog(OF_LOG_WARNING, "makeDirectory - directory already exists");
+//
+//		return success;
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static bool deleteFile(string path, bool bRelativeToData = true){
+//	    if( bRelativeToData ) path = ofToDataPath(path);
+//		File myFile(path);
+//		try{
+//            myFile.remove();
+//		}catch( Poco::Exception &except){
+//            ofLog(OF_LOG_ERROR, "deleteFile - file could not be deleted");
+//            return false;
+//		}
+//		return true;
+//	}
+//
+//	//------------------------------------------------------------------------------------------------------------
+//	static bool deleteFolder(string path, bool bRelativeToData = true){
+//	    if( bRelativeToData ) path = ofToDataPath(path);
+//		File myFile(path);
+//		try{
+//            myFile.remove(true);
+//		}catch(Poco::Exception &except){
+//            ofLog(OF_LOG_ERROR, "deleteFile - folder could not be deleted");
+//            return false;
+//		}
+//		return true;
+//	}
+//
+//};
